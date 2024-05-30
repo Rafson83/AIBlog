4602d2875eed23101adc46e049e310bb4674d95f
Rafa Wielgus <wielgus.rafal@gmail.com>
2024-05-30 16:55:32 +0200

    Kontunuacja zmiany szkieletu

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Custom JavaScript here
+// js/scripts.js
+
+const menuToggle = document.querySelector('#mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+menuToggle.addEventListener('click', function() {
+    navList.classList.toggle('active');
 });
